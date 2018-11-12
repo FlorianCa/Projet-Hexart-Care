bdc449ac9bfc02821609063bec237c079ebabbdf
FlorianCa <44927341+FlorianCa@users.noreply.github.com>
2018-11-12 11:09:54 +0100

    test

--- Module 1/main.c/main.c.ino
+++ Module 1/main.c/main.c.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+int valeurPrecedente=0; //valeur précédente du poul, pour vérifier qu'on entre dans un cycle
+long tempsPrecedent=0; //valeur précédente du temps
 
+void setup() {
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+int valeurActuelle, valeurSeuil;
+  long tempsDetection;
+
+  valeurActuelle = analogRead(0);
+  valeurSeuil = 650;
+
+  if (valeurActuelle > valeurSeuil) {  // on est dans la zone max
+    if (valeurPrecedente <= valeurSeuil) {  // est-ce qu'on vient d'y entrer?
+      tempsDetection = millis();
+      if (tempsDetection > (tempsPrecedent + 200)){  // ce n'est pas seulement du bruit?
+        Serial.println( (1000.0 * 60.0) / (tempsDetection - tempsPrecedent),0);
+        tempsPrecedent = tempsDetection;
+      }
+    }
+  }
+
+  valeurPrecedente = valeurActuelle;
+
 
 }
