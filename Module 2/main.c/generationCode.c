f04ce083517e570507e1d2bdaabb758c85320848
Avantro <44925290+JLAvantro@users.noreply.github.com>
2018-11-13 10:04:08 +0100

    Up module 3.2 partie 3

--- Module 2/main.c/generationCode.c
+++ Module 2/main.c/generationCode.c
@@ -5,23 +5,7 @@
 
     FILE *f;
 
-int main(int argc, char *argv[])
-{
-
-    fichier = fopen("param.txt", "r+");
-
-    if (fichier==NULL)
-    {
-        printf("Le fichier n'existe pas\n")
-    }
-    else
-    {
-        lancer param.h
-    }
-    return 0;
-}
-
-void Mode_BCoeur();
+    void Mode_BCoeur();
 {
     fopen("coeur.txt", "w+");
         if (fichier==NULL)
@@ -30,26 +14,7 @@ void Mode_BCoeur();
     }
     else
     {
-        fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,HIGH)\n
-    digitalWrite(LED_4,HIGH);
-    digitalWrite(LED_5,HIGH);
-    digitalWrite(LED_6,HIGH);
-    digitalWrite(LED_7,HIGH);
-    digitalWrite(LED_8,HIGH);
-    digitalWrite(LED_9,HIGH);
-    digitalWrite(LED_10,HIGH);
-        delay(857);
-    digitalWrite(LED_1,LOW);
-    digitalWrite(LED_2,LOW);
-    digitalWrite(LED_3,LOW);
-    digitalWrite(LED_4,LOW);
-    digitalWrite(LED_5,LOW);
-    digitalWrite(LED_6,LOW);
-    digitalWrite(LED_7,LOW);
-    digitalWrite(LED_8,LOW);
-    digitalWrite(LED_9,LOW);
-    digitalWrite(LED_10,LOW);
-       delay(857);")
+        fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,HIGH)\n digitalWrite(LED_4,HIGH)\n digitalWrite(LED_5,HIGH)\n digitalWrite(LED_6,HIGH)\n digitalWrite(LED_7,HIGH)\n digitalWrite(LED_8,HIGH)\n digitalWrite(LED_9,HIGH)\n digitalWrite(LED_10,HIGH)\n delay(857)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,LOW)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_10,LOW)\n delay(857)\n")
     }
     return 0;
 }
