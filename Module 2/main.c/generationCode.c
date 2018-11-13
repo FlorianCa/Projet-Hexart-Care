25cafa8edaa8b939f926663f0d7c5e7efba5178f
Avantro <44925290+JLAvantro@users.noreply.github.com>
2018-11-13 11:12:26 +0100

    generationCode.c + param.h

--- Module 2/main.c/generationCode.c
+++ Module 2/main.c/generationCode.c
@@ -3,18 +3,42 @@
 #include "menu.h"
 #include "generationCode.h"
 
-    FILE *f;
+int LED_auChoix;
+FILE* fichier
 
-    void Mode_BCoeur();
+void Mode_bCoeur();
 {
-    fopen("coeur.txt", "w+");
-        if (fichier==NULL)
-    {
-        printf("Le fichier n'existe pas\n")
-    }
-    else
-    {
-        fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,HIGH)\n digitalWrite(LED_4,HIGH)\n digitalWrite(LED_5,HIGH)\n digitalWrite(LED_6,HIGH)\n digitalWrite(LED_7,HIGH)\n digitalWrite(LED_8,HIGH)\n digitalWrite(LED_9,HIGH)\n digitalWrite(LED_10,HIGH)\n delay(857)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,LOW)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_10,LOW)\n delay(857)\n")
-    }
-    return 0;
+    fichier = fopen("param.h", "w+");
+    fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,HIGH)\n digitalWrite(LED_4,HIGH)\n digitalWrite(LED_5,HIGH)\n digitalWrite(LED_6,HIGH)\n digitalWrite(LED_7,HIGH)\n digitalWrite(LED_8,HIGH)\n digitalWrite(LED_9,HIGH)\n digitalWrite(LED_10,HIGH)\n delay(857)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,LOW)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_10,LOW)\n delay(857)\n", fichier);
+    fclose(fichier);
+}
+
+void Mode_unSurDeux();
+{
+    fichier = fopen("param.h", "w+");
+    fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,HIGH)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,HIGH)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,HIGH)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_9,HIGH)\n  digitalWrite(LED_1,LOW)\n delay(857)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,LOW)digitalWrite(LED_4,HIGH)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,HIGH)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,HIGH)\n digitalWrite(LED_9,LOW)\n digitalWrite(LED_10,HIGH)\n delay(857)\n", fichier);
+    fclose(fichier);
+}
+
+void Mode_unSurTrois();
+{
+    fichier = fopen("param.h", "w+");
+    fputs("digitalWrite(LED_1,HIGH)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,LOW)\n digitalWrite(LED_4,HIGH)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,HIGH)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_9,LOW)\n digitalWrite(LED_10,HIGH)\n delay(857)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,HIGH)\n digitalWrite(LED_3,HIGH)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,HIGH)\n digitalWrite(LED_6,HIGH)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,HIGH)\n digitalWrite(LED_9,HIGH)\n digitalWrite(LED_10,LOW)\n delay(857)\n", fichier);
+    fclose(fichier);
+}
+
+void Mode_auChoix();
+{
+    printf("Led au choix\n")
+    scanf("%i",&LED_auChoix)
+    fichier = fopen("param.h", "w+");
+    fputs("digitalWrite(LED_auChoix,HIGH)\n", fichier);
+    fclose(fichier);
+}
+
+void Mode_chenille();
+{
+    fichier = fopen("param.h", "w+");
+    fputs("digitalWrite(LED_1,HIGH)\n delay(200)\n digitalWrite(LED_1,LOW)\n digitalWrite(LED_2,HIGH)\n delay(200)\n digitalWrite(LED_2,LOW)\n digitalWrite(LED_3,HIGH)\n delay(200)\n digitalWrite(LED_3,LOW)\n digitalWrite(LED_4,HIGH)\n delay(200)\n digitalWrite(LED_4,LOW)\n digitalWrite(LED_5,HIGH)\n delay(200)\n digitalWrite(LED_5,LOW)\n digitalWrite(LED_6,HIGH)\n delay(200)\n digitalWrite(LED_6,LOW)\n digitalWrite(LED_7,HIGH)\n delay(200)\n digitalWrite(LED_7,LOW)\n digitalWrite(LED_8,HIGH)\n delay(200)\n digitalWrite(LED_8,LOW)\n digitalWrite(LED_9,HIGH)\n delay(200)\n digitalWrite(LED_9,LOW)\n digitalWrite(LED_10,HIGH)\n delay(200)\n digitalWrite(LED_10,LOW)\n", fichier);
+    fclose(fichier);
 }
