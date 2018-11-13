e373bd82f95121f13a0beab631c4bc4857bbeecc
AdrienKawak <33150132+AdrienKawak@users.noreply.github.com>
2018-11-13 13:58:27 +0100

    Module 4
    Commencement réalisation module 4

---
+++ Module 4/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>      //Inclue la bibliotheque stdio.h
+#include <stdlib.h>     //Inclue la bibliotheque stdlib.h
+#include <string.h>     //Inclue la bibliotheque
+
+#include "donnees.h"    //Inclue le fichier données.h
+#include "menu.h"       //Inclue le fichier menu.h
+#include "action.h"     //Inclue le fichier action.h
+
+//Creation de la fonction main()
+int main()
+{
+    menu(); //Appel de la fonction menu()
+    return 0;
+}
