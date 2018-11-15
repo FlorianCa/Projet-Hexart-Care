65cd52e40e362950e52851789a844bc2ccc615ae
AdrienKawak <33150132+AdrienKawak@users.noreply.github.com>
2018-11-15 10:51:29 +0100

    Module 1
    Modification code pour la mesure du pouls

--- Module 1/main.c/main.c.ino
+++ Module 1/main.c/main.c.ino
@@ -1,19 +1,40 @@
 #include "cardio.h"
 
-int valeurPrecedente = 0;
-long tempsPrecedent = 0;
+//  VARIABLES
+int CapteurPouls = 0;// Capteur Pouls connecté au Pin0
+int Coeur = 13; //  Controle LED Rouge
 
-void setup()
-{
-  Serial .begin(9600);
+int TabPinLed[10]={2,3,4,5,6,7,8,9,10,11}; //Création d'un tableau de 10 valeurs où l'on a attribué les différents pin
+int i;
+
+double Signal; // Contient les données brutes et elles peuvent aller de 0 à 1023
+float ValeurSeuil = 2.5; // Permet de détecter quel battement choisir et lequel ignorer avec un seuil de 2.5 volt
+
+
+// FONCTION SETUP
+void setup() {
+   pinMode(Coeur,OUTPUT); // Fait en sorte que la LED clignote au rythme cardiaque
+   Serial.begin(9600);    
+   pinMode(2, OUTPUT);
+   pinMode(3, OUTPUT);
+   pinMode(4, OUTPUT);
+   pinMode(5, OUTPUT);
+   pinMode(6, OUTPUT);
+   pinMode(7, OUTPUT);
+   pinMode(8, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(10, OUTPUT);
+   pinMode(11, OUTPUT);     
 }
 
-void loop()
-{
-  int poul;
-  int valeurActuelle = 653; //analogRead(0);
+// FONCTION LOOP
+void loop() {
+   Signal = analogRead(CapteurPouls);  // Lit la valeur du pouls
+                                       // Et attribue la valeur à la variable signal
+                                      
+   Serial.print(Signal * 5 / 1023); //Affichage du signal converti en Volt
+   Serial.print(";"); //Affichage du ";"
+   Serial.println(millis()); //Affichage temps depuis lancement du programme
 
-  poul = calculPoul(valeurActuelle, valeurPrecedente, tempsPrecedent);
-  
-  Serial.println(poul);
+   Mode_bCoeur(); //Appel fonction Mode_BCoeur
 }
